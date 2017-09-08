dd3f80a6cd3d6e7a46da5f59f06d468df694bf45
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-09-08 15:17:17 +0100

    Testing React Components with Enzyme and Jest

---
+++ REACT/src/App.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import App from './App';
+import { mount, shallow } from 'enzyme';
+// shallow allows to see object structure
+//Mount allows to invoke method
+
+
+describe('<App/>', () => {
+	it('reders 1 <App/> component', () => {
+		const component = shallow(<App name="app" />);
+		//console.log(component.props);
+		expect(component).toHaveLength(1);
+	});
+	it('it renders props correctly', ()=> {
+		const component = shallow(<App name="app" />);
+		//console.log(component.instance().props);
+		expect(component.instance().props.name).toBe('app');
+	});
+	it('it updates the counter on button click', () => {
+		const component = mount(<App />);
+		//console.log(component);
+		const button = component.find('button');
+		//console.log(button.props); output :: //{onclick=[function], children: 'Increment'}
+		
+		
+		button.simulate('click');
+		button.simulate('click');
+		button.simulate('click');
+		button.simulate('click');
+		button.simulate('click');
+		//console.log(component.state());
+		//expect(component.state().counter).toEqual(5);
+		expect(component.state().counter).toEqual(15
+	})
+});
